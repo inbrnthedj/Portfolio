39599fce0b147616c3c337dada533f6bf6cdb354
Iane De Jesus <inbrnthedj@gmail.com>
2026-05-14 17:45:22 +0200

    Updated and added Python codes

--- PythonPractices/Python-03_ProgrammingFundamentals/A_Quiz-01_VerifyAchievements.py
+++ PythonPractices/Python-03_ProgrammingFundamentals/A_Quiz-01_VerifyAchievements.py
@@ -1,15 +1,15 @@
 ''' Here's the data for the quiz:
 
-| Player Name | Sport | Achievements |
-|-------------|-------|--------------|
-| Serena Williams | Tennis | 23 Grand Slam titles |
-| Lionel Messi | Football | 7 Ballon d'Or awards |
-| Michael Phelps | Swimming | 23 Olympic gold medals |
-| Usain Bolt | Athletics | 8 Olympic gold medals |
-| Simone Biles | Gymnastics | 7 Olympic medals (4 gold) |
-| Roger Federer | Tennis | 20 Grand Slam titles |
-| Cristiano Ronaldo | Football | 5 Ballon d'Or awards |
-| Katie Ledecky | Swimming | 7 Olympic gold medals |
+| Player Name       | Sport     | Achievements              |
+|-------------------|-----------|---------------------------|
+| Serena Williams   | Tennis    | 23 Grand Slam titles      |
+| Lionel Messi      | Football  | 7 Ballon d'Or awards      |
+| Michael Phelps    | Swimming  | 23 Olympic gold medals    |
+| Usain Bolt        | Athletics | 8 Olympic gold medals     |
+| Simone Biles      | Gymnastics| 7 Olympic medals (4 gold) |
+| Roger Federer     | Tennis    | 20 Grand Slam titles      |
+| Cristiano Ronaldo | Football  | 5 Ballon d'Or awards      |
+| Katie Ledecky     | Swimming  | 7 Olympic gold medals     |
 
 TASK 1: Write a Python program to check if a player Lionel Messi has more than 10 achievements.
         If the condition is true, print the player's name and achievements. 
